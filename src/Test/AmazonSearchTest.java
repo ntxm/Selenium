f0748b7bfaa78d3c9265066dde19d324f8f42e48
ntxm <51963319+ntxm@users.noreply.github.com>
2019-12-25 18:18:04 -0500

    new steps added

--- src/Test/AmazonSearchTest.java
+++ src/Test/AmazonSearchTest.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
-public class AmazonOrderTest {
+public class AmazonSearchTest {
 
 	public static void main(String[] args) throws InterruptedException {
 		
@@ -29,21 +29,26 @@ public class AmazonOrderTest {
 		//find product
 		driver.findElement(By.xpath("//input[@id = 'twotabsearchtextbox']")).sendKeys("perrier sparkling water");
 		driver.findElement(By.xpath("//input[@value = 'Go']")).click();
-		Thread.sleep(3000);
+		Thread.sleep(1000);
 		
 		//Sort search button
 		driver.findElement(By.xpath("//span[@id='a-autoid-0-announce']")).click();
 		driver.findElement(By.xpath("//a[@id='s-result-sort-select_1']")).click();
-		Thread.sleep(2000);
-		
-		
-		
+		Thread.sleep(1000);
 		
 		//CheckBox --> search filters
-		WebElement checkBox = driver.findElement(By.xpath("//i[@aria-label='Prime Eligible']"));
-		checkBox.click();
-		WebElement checkBox1 = driver.findElement(By.xpath("//li[@id='p_89/Perrier']"));
+	 	WebElement checkBox = driver.findElement(By.xpath("//li[@id='p_85/2470955011']//i[@class='a-icon a-icon-checkbox']"));
+	    checkBox.click();
+	 
+		WebElement checkBox1 = driver.findElement(By.xpath("//li[@id='p_89/Perrier']//i[@class='a-icon a-icon-checkbox']"));
 		checkBox1.click();
+		Thread.sleep(1000);
+		
+		//Add to Cart first position
+		driver.findElement(By.xpath("//span[@class='celwidget slot=SEARCH_RESULTS template=SEARCH_RESULTS widgetId=search-results index=0']//img[@class='s-image']")).click();
+		
+		//Drop-down menu --> change quantity
+		//driver.findElement(By.xpath("//span[@id='a-autoid-0-announce']")).click();
 		
 		
 		Thread.sleep(5000);
